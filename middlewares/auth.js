e56b14749cc1d44fcc2f498fe808048b63025787
stockology <124348998+stockology@users.noreply.github.com>
2023-03-07 16:43:41 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,7 +6,7 @@ import { User } from "../models/User.js";
 export const isAuthenticated = catchAsyncError(async (req, res, next) => {
   const { token } = req.cookies;
 
-  if (!token) return next(new ErrorHandler("Not Logged In", 401));
+ 
 
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
